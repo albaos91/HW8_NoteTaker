ef72e97fa4228d43118f3a89bdf1933c4f4d55bb
Alba Osmanaj <albaos91@gmail.com>
2020-07-14 21:20:04 -0500

    created html route

---
+++ routes/html_routes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function (app) {
+  //Html get request to show notes.html
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  //Html get request to show index.html
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
