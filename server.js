96abb0e20849fb00c69d1f5fc2295d7823594b70
Alba Osmanaj <albaos91@gmail.com>
2020-07-23 18:56:06 -0500

    created server.js, app runs now

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+// ======================================================
+
+var express = require("express");
+
+// Express Startup
+var app = express();
+var PORT = process.env.port || 3000;
+
+// Express handling
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/api_routes")(app);
+require("./routes/html_routes")(app);
+
+// Start server and begin listening
+app.listen(PORT, function () {
+  console.log(`Server listening on port:${PORT}`);
+});
