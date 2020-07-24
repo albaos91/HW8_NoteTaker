e6830ba834b92c3a82f41e153a7001cbbd4992b5
Alba Osmanaj <albaos91@gmail.com>
2020-07-23 19:22:11 -0500

    fixed PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 
 // Express Startup
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Express handling
 app.use(express.urlencoded({ extended: true }));
